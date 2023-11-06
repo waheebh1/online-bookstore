36246c3120be814960780d4ce5a103115720ee3a
maishabd23 <abdullah.maisha@yahoo.ca>
2023-11-05 20:08:28 -0500

    Changed package name and added .gitignore.

---
+++ project9/src/main/java/bookstore/App.java
@@ -0,0 +1,18 @@
+package bookstore;
+
+/*
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
