592be98763c193161b23c34acdc4675868a7a4b3
Sabah Samwatin <90452719+sabahsamwatin@users.noreply.github.com>
2023-11-09 13:31:02 -0500

    Added Log-in Functionality to UserController.java

--- src/main/java/bookstore/users/UserController.java
+++ src/main/java/bookstore/users/UserController.java
@@ -1,11 +1,12 @@
 package bookstore.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -32,5 +33,35 @@ public class UserController {
         model.addAttribute("user", bookUser);
 
         return "success";
+    }
+
+    @GetMapping("/account")
+    public String accountForm (Model model){
+        model.addAttribute("user", new BookUser());
+        return "accountForm"; // one form for both account creation and login
+    }
+    @PostMapping("/account")
+    public String handleUserLoginOrCreation(@ModelAttribute BookUser formUser, Model model) {
+        // Check if user exists
+        List<BookUser> existingUsers = userRepository.findByUsername(formUser.getUsername());
+
+        if (existingUsers.isEmpty()) {
+            // No existing user, create a new one
+            userRepository.save(formUser);
+            model.addAttribute("user", formUser);
+            return "accountCreated"; // Redirect to an account created confirmation page
+        } else {
+            // User exists, check password
+            BookUser existingUser = existingUsers.get(0); // Assuming unique usernames
+            if (existingUser.getPassword().equals(formUser.getPassword())) {
+                // Passwords match, login successful
+                model.addAttribute("user", existingUser);
+                return "userProfile"; // Redirect to user profile page
+            } else {
+                // Passwords do not match, return login error
+                model.addAttribute("loginError", "Invalid password");
+                return "accountForm"; // Stay on the login/create account page
+            }
+        }
     }
 }
