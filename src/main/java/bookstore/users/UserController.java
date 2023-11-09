84c51394d336e90fc085f56eaaacc5bfbf0d0672
Thanuja <thanujaca@gmail.com>
2023-11-09 08:55:41 -0500

    Implement methods and html for creating new user.

--- src/main/java/bookstore/users/UserController.java
+++ src/main/java/bookstore/users/UserController.java
@@ -7,30 +7,62 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * User controller
+ * @author Thanuja Sivaananthan
+ * @author Sabah Samwatin
+ */
 @Controller
 public class UserController {
 
     private final UserRepository userRepository;
 
 
+    /**
+     * Create new user controller
+     * @author Thanuja Sivaananthan
+     * @param userRepository   user repository
+     */
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Create new account
+     * @author Thanuja Sivaananthan
+     * @param model model to use
+     * @return form page
+     */
     @GetMapping("/createAccount")
     public String createAccountForm(Model model) {
+        BookUser bookUser = new BookUser();
 
-        model.addAttribute("user", new BookUser());
+        model.addAttribute("user", bookUser);
         return "createAccountForm";
     }
 
+    /**
+     * Submit new account
+     * @author Thanuja Sivaananthan
+     * @param bookUser  user to add
+     * @param model     model
+     * @return          result page
+     */
     @PostMapping("/createAccount")
     public String createAccountSubmit(@ModelAttribute BookUser bookUser,
                                        Model model)
     {
+
+        if (bookUser.getUserType().equals("O")){
+            BookOwner bookOwner = new BookOwner(bookUser.getId(), bookUser.getUsername(), bookUser.getPassword());
+            bookUser = bookOwner;
+        } else {
+            bookUser.setUserType(UserType.BOOKUSER.toString());
+        }
         userRepository.save(bookUser);
+
         model.addAttribute("user", bookUser);
 
-        return "success";
+        return "createAccountResult";
     }
 }
