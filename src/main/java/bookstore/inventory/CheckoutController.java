d721365def3e0b28c3072bf67e0957732c1fd1b5
shrimeichock <meichock@gmail.com>
2023-11-11 10:38:09 -0500

    Fix display books endpoint to use inventory object

--- src/main/java/bookstore/inventory/CheckoutController.java
+++ src/main/java/bookstore/inventory/CheckoutController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class CheckoutController {
 
@@ -14,17 +11,19 @@ public class CheckoutController {
     private final BookRepository bookRepository;
     private final ShoppingCartRepository shoppingCartRepository;
     private final InventoryRepository inventoryRepository;
+    private final InventoryItemRepository inventoryItemRepository;
 
     /**
      * Constructor for checkout controller
      * @param authorRepo repository of authors
      * @param bookRepo repository of books
      */
-    public CheckoutController(AuthorRepository authorRepo, BookRepository bookRepo, ShoppingCartRepository cartRepo, InventoryRepository inventoryRepo){
+    public CheckoutController(AuthorRepository authorRepo, BookRepository bookRepo, ShoppingCartRepository cartRepo, InventoryRepository inventoryRepo, InventoryItemRepository inventoryItemRepo){
         this.authorRepository = authorRepo;
         this.bookRepository = bookRepo;
         this.shoppingCartRepository = cartRepo;
         this.inventoryRepository = inventoryRepo;
+        this.inventoryItemRepository = inventoryItemRepo;
     }
 
     /**
@@ -32,21 +31,10 @@ public class CheckoutController {
      * @param model container
      * @return reroute to html page to display all books
      */
-    @GetMapping("/books")
+    @GetMapping("/listAvailableBooks")
     public String listAvailableBooks(Model model) {
-        ArrayList<Author> author_list = new ArrayList<>();
-        Author author1 = new Author("Harper", "Lee");
-        author_list.add(author1);
-
-        String description = "Compassionate, dramatic, and deeply moving, To Kill A Mockingbird takes readers to the roots of human behavior - to innocence and experience, kindness and cruelty, love and hatred, humor and pathos.";
-        Book book1 = new Book("0446310786", "To Kill a Mockingbird", "https://m.media-amazon.com/images/W/AVIF_800250-T2/images/I/71FxgtFKcQL._SL1500_.jpg", author_list, "Grand Central Publishing", "Classical", 12.99, description);
-        author1.addBook(book1); //add to bibliography
-        //authorRepository.save(author1);
-        //bookRepository.save(book1);
-
-        List<Book> books = (List<Book>) bookRepository.findAll();
-        System.out.println(books);
-        model.addAttribute("books", books);
+        Iterable<InventoryItem> inventory = inventoryItemRepository.findAll();
+        model.addAttribute("inventory", inventory);
         return "home";
     }
 
