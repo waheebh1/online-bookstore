dbb040389547fbe2159e43dafffcd150325676b0
shrimeichock <meichock@gmail.com>
2023-11-11 15:49:54 -0500

    Add other search options for books

--- src/main/java/bookstore/inventory/BookRepository.java
+++ src/main/java/bookstore/inventory/BookRepository.java
@@ -7,8 +7,11 @@ package bookstore.inventory;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface BookRepository extends CrudRepository<Book, String>{
-    Book findByIsbn(String isbn);
-    ArrayList<Book> findByTitle(String title);
+    Book findBIsbn(String isbn);
+    List<Book> findByTitle(String title);
+    List<Book> findByGenre(String genre);
+    List<Book> findByAuthor(ArrayList<Author> author);
 }
