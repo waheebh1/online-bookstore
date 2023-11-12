1cef20e775fc1a9d4df52d8354b672be149bf78e
shrimeichock <meichock@gmail.com>
2023-11-12 17:32:45 -0500

    updated totalPrice attribute in ShoppingCart to use Double. Rearranged order of parameters in Book constructor for consistency

--- src/test/java/bookstore/inventory/InventoryItemTest.java
+++ src/test/java/bookstore/inventory/InventoryItemTest.java
@@ -26,14 +26,14 @@ public class InventoryItemTest {
         author_list.add(author1);
 
         this.description1 = "Compassionate, dramatic, and deeply moving, To Kill A Mockingbird takes readers to the roots of human behavior - to innocence and experience, kindness and cruelty, love and hatred, humor and pathos.";
-        Book book1 = new Book("0446310786", "To Kill a Mockingbird", "11/07/1960", "https://m.media-amazon.com/images/W/AVIF_800250-T2/images/I/71FxgtFKcQL._SL1500_.jpg", author_list, "Grand Central Publishing", "Classical", 12.99, description1);
+        Book book1 = new Book("0446310786", "To Kill a Mockingbird", author_list, 12.99, "11/07/1960", "https://m.media-amazon.com/images/W/AVIF_800250-T2/images/I/71FxgtFKcQL._SL1500_.jpg", "Grand Central Publishing", "Classical", description1);
         this.item1 = new InventoryItem(book1, 5);
 
         ArrayList<Author> author_list2 = new ArrayList<>();
         Author author2 = new Author("Khaled", "Hosseini");
         author_list2.add(author2);
         this.description2 = "The Kite Runner tells the story of Amir, a young boy from the Wazir Akbar Khan district of Kabul";
-        this.book2 = new Book("1573222453", "The Kite Runner", "29/05/2003", "https://upload.wikimedia.org/wikipedia/en/6/62/Kite_runner.jpg", author_list2,"Riverhead Books", "Historical fiction", 22.00, description2);
+        this.book2 = new Book("1573222453", "The Kite Runner", author_list2, 22.00, "29/05/2003", "https://upload.wikimedia.org/wikipedia/en/6/62/Kite_runner.jpg", "Riverhead Books", "Historical fiction", description2);
     }
 
     @Test
