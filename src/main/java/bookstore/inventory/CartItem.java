4b93c27e57261cccf89b1908d3a7186249fca281
maishabd23 <abdullah.maisha@yahoo.ca>
2023-11-21 14:13:36 -0500

    javadoc on CartItem

--- src/main/java/bookstore/inventory/CartItem.java
+++ src/main/java/bookstore/inventory/CartItem.java
@@ -4,6 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+/**
+ * Cart Item Object
+ * @author Maisha Abdullah
+ */
+
 @Entity
 public class CartItem extends Item {
 
@@ -11,23 +16,46 @@ public class CartItem extends Item {
     @JoinColumn(name = "shopping_cart_id")
     private ShoppingCart shoppingCart;
 
+    /**
+     * Default constructor
+     */
     public CartItem(){
 
     }
 
+    /**
+     * Constructor for CartItem
+     * @param book      Book that is the item
+     * @param quantity  Quantity of that item
+     */
     public CartItem(Book book, int quantity){
         super(book, quantity);
     }
 
+
+    /**
+     * Constructor for CartItem
+     * @param book book
+     * @param quantity quantity
+     * @param shoppingCart shopping cart
+     */
     public CartItem (Book book, int quantity, ShoppingCart shoppingCart){
         super(book,quantity);
         this.shoppingCart = shoppingCart;
     }
 
+    /**
+     * Getter method for shopping cart
+     * @return shopping cart
+     */
     public ShoppingCart getShoppingCart() {
         return shoppingCart;
     }
 
+    /**
+     * Setter method for shopping cart
+     * @param shoppingCart shopping cart
+     */
     public void setShoppingCart(ShoppingCart shoppingCart) {
         this.shoppingCart = shoppingCart;
     }
