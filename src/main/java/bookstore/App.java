6adf2efbffe7bd2f5860e4c4ffa6918ddb9cdbdb
Sabah Samwatin <90452719+sabahsamwatin@users.noreply.github.com>
2023-12-05 21:15:58 -0500

    Fixed Date format

--- src/main/java/bookstore/App.java
+++ src/main/java/bookstore/App.java
@@ -46,7 +46,7 @@ public class App
             author_list1.add(author1);
 
             String description1 = "Compassionate, dramatic, and deeply moving, To Kill A Mockingbird takes readers to the roots of human behavior - to innocence and experience, kindness and cruelty, love and hatred, humor and pathos.";
-            book1 = new Book("0446310786", "To Kill a Mockingbird", author_list1, 12.99, "11/07/1960", "https://m.media-amazon.com/images/W/AVIF_800250-T2/images/I/71FxgtFKcQL._SL1500_.jpg", "Grand Central Publishing", "Classical", description1);
+            book1 = new Book("0446310786", "To Kill a Mockingbird", author_list1, 12.99, "1960-07-11", "https://m.media-amazon.com/images/W/AVIF_800250-T2/images/I/71FxgtFKcQL._SL1500_.jpg", "Grand Central Publishing", "Classical", description1);
             author1.addBook(book1); //add to bibliography
             authorRepo.save(author1);
             bookRepo.save(book1);
@@ -58,14 +58,14 @@ public class App
             author_list2.add(author2);
 
             String description2 = "The Kite Runner tells the story of Amir, a young boy from the Wazir Akbar Khan district of Kabul";
-            book2 = new Book("1573222453", "The Kite Runner", author_list2, 22.00, "29/05/2003", "https://upload.wikimedia.org/wikipedia/en/6/62/Kite_runner.jpg", "Riverhead Books", "Historical fiction", description2);
+            book2 = new Book("1573222453", "The Kite Runner", author_list2, 22.00, "2003-05-29", "https://upload.wikimedia.org/wikipedia/en/6/62/Kite_runner.jpg", "Riverhead Books", "Historical fiction", description2);
             authorRepo.save(author2);
             bookRepo.save(book2);
             item2 = new InventoryItem(book2, 10, inventory);
             itemRepository.save(item2);
 
             String description3 = "Twenty-six-year-old Jean Louise Finch returns home to Maycomb, Alabama from New York City to visit her aging father, Atticus.";
-            book3 = new Book("978-0-06-240985-0", "Go Set a Watchman", author_list1, 14.99, "14/07/2015", "https://m.media-amazon.com/images/I/61lFywIUwzL.jpg", "Harper Collins", "Historical fiction", description3);
+            book3 = new Book("978-0-06-240985-0", "Go Set a Watchman", author_list1, 14.99, "2015-07-14", "https://m.media-amazon.com/images/I/61lFywIUwzL.jpg", "Harper Collins", "Historical fiction", description3);
             bookRepo.save(book3);
             item3 = new InventoryItem(book3, 2, inventory);
             itemRepository.save(item3);
